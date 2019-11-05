3788f73c3070c5d20d5b288db91bc7d46361a570
Chukwuma Mabi <chuks.mabi@yahoo.com>
2019-11-05 05:33:05 +0100

    update personal expense

--- personal_expense/lib/widgets/transaction_list.dart
+++ personal_expense/lib/widgets/transaction_list.dart
@@ -25,10 +25,11 @@ class TransactionList extends StatelessWidget {
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
-                            color: Colors.purple),
+                            color: Theme.of(context).primaryColor),
                       ),
                       decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2),
+                        border: Border.all(color: Theme.of(context).primaryColorDark, width: 2),
+
                       ),
                       padding: EdgeInsets.all(10),
                     ),
